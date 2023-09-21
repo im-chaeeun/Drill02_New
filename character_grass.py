e69de30bf11eb9ee90b8aeb7ba07704cebc90411
2020180038_hyeyeon <112497688+yeonii01@users.noreply.github.com>
2023-09-21 12:36:35 +0900

    사각 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -25,12 +25,20 @@ def run_rectangle():
     print('RECTANGLE')
 
     #bottom line
-    #for x in range(50, 750+1,5):
-        #render_frame(x, 90) # x, y 위치에 캐릭터를 그려줄 수 있는
+    for x in range(50, 750+1,5):
+        render_frame(x, 90) # x, y 위치에 캐릭터를 그려줄 수 있는
+
+    #right line
+    for y in range(90, 550, 5):
+        render_frame(750, y)
         
     #top line
     for x in range(750, 50-1, -5):
         render_frame(x, 550)
+
+    #left line
+    for y in range(550, 90, -5):
+        render_frame(50, y)   
 
 while True:
     #run_circle()
