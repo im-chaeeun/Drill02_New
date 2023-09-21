3a15fe57707b73f4af8ed197403cdd93b0ce7623
2020180038_hyeyeon <112497688+yeonii01@users.noreply.github.com>
2023-09-21 12:14:46 +0900

    바닥줄 이동

--- character_grass.py
+++ character_grass.py
@@ -1,11 +1,11 @@
-from pico2d import *
 import math
+from pico2d import *
 
-open_canvas()
-
+open_canvas(800, 600)
 character=load_image('character.png')
 grass=load_image('grass.png')
-clear_canvas_now(400,30)
+
+clear_canvas_now()
 grass.draw_now(400, 90)
 delay(1)
 
@@ -16,18 +16,25 @@ def run_circle():
     for deg in range(0, 360, 5):
         x=cx+r*math.cos(math.radians(deg))
         y=cy+r*math.sin(math.radians(deg))
-        clear_canvas_now(400,30)
-        grass.draw_now(400, 90)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
         character.draw_now(x, y)
         delay(0.1)
     pass
 
 def run_rectangle():
     print('RECTANGLE')
-    pass
+
+    #bottom line
+    for x in range(50, 750+1,5):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        delay(0.1)
+        
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break;
 
